b1a62de7983619f23d4a002661905c91c261655e
lucas <lcs_demais@hotmail.com>
2023-06-12 20:32:51 -0300

    att

--- app-calculo/src/main/java/br/com/lucas/appCalculo/Calculadora.java
+++ app-calculo/src/main/java/br/com/lucas/appCalculo/Calculadora.java
@@ -1,5 +1,6 @@
 package br.com.lucas.appCalculo;
 
+import br.com.lucas.appLogging.Logger;
 import br.com.lucas.interno.OperacoesAritmeticas;
 
 public class Calculadora {
@@ -7,6 +8,7 @@ public class Calculadora {
     private OperacoesAritmeticas opAritmeticas = new OperacoesAritmeticas();
 
     public double soma(double... nums) {
+        Logger.info("Somando...");
         return opAritmeticas.soma(nums);
     }
 }
--- app-calculo/src/main/java/module-info.java
+++ app-calculo/src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module app.calculo{
     exports br.com.lucas.appCalculo;
+    requires app.logging;
 }
